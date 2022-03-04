17be3510408d154e2b9997fb4e8a0f7062a5f45c
timble <timblemichael20@gmail.com>
2022-03-04 18:20:35 +1000

    17-passes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Your code here
-
 function saturdayFun(){
     return ""
 }
@@ -7,5 +6,60 @@ function saturdayFun(){
 function saturdayFun(activity="roller-skate"){
     return `This Saturday, I want to ${activity}!`
 }
+
+function mondayWork(){
+
+}
+
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(){
+    return ""
+}
+
+const Calculator = {
+    add: function(a,b){
+        return a+b
+    },
+    subtract: function(a,b){
+        return a-b
+    },
+    multiply: function(a,b){
+        return a*b
+    },
+    divide: function(a,b){
+        return a/b
+    }
+}
+
+function actionApplyer(){
+    return ""
+}
+
+
+array = [integer*2, integer+100,integer%7]
+
+function actionApplyer(integer,array){
+    for(element of array){
+        return integer
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
